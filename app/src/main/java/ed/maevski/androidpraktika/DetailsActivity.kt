564fc34e66ce540de20d1408382493578b2d916c
maevskiEd <maevski.ed@gmail.com>
2022-12-13 23:03:35 +0300

    finish DetailsActivity_

--- app/src/main/java/ed/maevski/androidpraktika/DetailsActivity.kt
+++ app/src/main/java/ed/maevski/androidpraktika/DetailsActivity.kt
@@ -4,7 +4,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ed.maevski.androidpraktika.data.DeviantPicture
 import ed.maevski.androidpraktika.databinding.ActivityDetailsBinding
-import ed.maevski.androidpraktika.databinding.ActivityMainBinding
 
 class DetailsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityDetailsBinding

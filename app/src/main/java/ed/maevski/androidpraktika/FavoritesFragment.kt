1c2191e731b7196b881faeb1d3e0584630a480d1
maevskiEd <maevski.ed@gmail.com>
2022-12-19 14:16:50 +0300

    module27 add search

--- app/src/main/java/ed/maevski/androidpraktika/FavoritesFragment.kt
+++ app/src/main/java/ed/maevski/androidpraktika/FavoritesFragment.kt
@@ -5,12 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import ed.maevski.androidpraktika.data.Item
 import ed.maevski.androidpraktika.databinding.FragmentFavoritesBinding
 import ed.maevski.androidpraktika.databinding.FragmentHomeBinding
 
-class FavoritesFragment : Fragment() {
+class FavoritesFragment(val devPictures: List<Item>) : Fragment() {
     private var _binding: FragmentFavoritesBinding? = null
     private val binding get() = _binding!!
+    private val favPictures = mutableListOf<Item>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,9 +25,9 @@ class FavoritesFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //Получаем список при транзакции фрагмента
-/*        val favoritesList: List<Film> = emptyList()
+        val favPictures: List<Item> = devPictures.filter { it.isInFavorites }
 
-        favorites_recycler
+/*         favorites_recycler
             .apply {
                 filmsAdapter =
                     FilmListRecyclerAdapter(object : FilmListRecyclerAdapter.OnItemClickListener {

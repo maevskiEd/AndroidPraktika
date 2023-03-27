203e23f8f62519b08ffdd9b52c82fbb065ef3ebb
maevskiEd <maevski.ed@gmail.com>
2023-03-27 14:22:44 +0300

    feature_42 finish

--- app/src/main/java/ed/maevski/androidpraktika/view/fragments/DetailsFragment.kt
+++ app/src/main/java/ed/maevski/androidpraktika/view/fragments/DetailsFragment.kt
@@ -1,18 +1,35 @@
 package ed.maevski.androidpraktika.view.fragments
 
+import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.bumptech.glide.Glide
+import com.google.android.material.snackbar.Snackbar
 import ed.maevski.androidpraktika.R
+import ed.maevski.androidpraktika.data.ApiConstants
 import ed.maevski.androidpraktika.data.entity.DeviantPicture
 import ed.maevski.androidpraktika.databinding.FragmentDetailsBinding
+import ed.maevski.androidpraktika.viewmodel.DetailsFragmentViewModel
+import ed.maevski.androidpraktika.viewmodel.HomeFragmentViewModel
+import kotlinx.coroutines.*
 
 class DetailsFragment : Fragment() {
     lateinit var picture: DeviantPicture
+    private val scope = CoroutineScope(Dispatchers.IO)
+    private val detailsFragmentViewModel: DetailsFragmentViewModel by viewModels()
 
     private var _binding: FragmentDetailsBinding? = null
     private val binding get() = _binding!!
@@ -47,6 +64,10 @@ class DetailsFragment : Fragment() {
         //Устанавливаем описание
         binding.detailsDescription.text = picture.description
 
+        binding.detailsFabDownloadWp.setOnClickListener {
+            performAsyncLoadOfPoster()
+        }
+
         binding.detailsFabFavorites.setOnClickListener {
             if (!picture.isInFavorites) {
                 binding.detailsFabFavorites.setImageResource(R.drawable.ic_baseline_favorite_24)
@@ -73,6 +94,113 @@ class DetailsFragment : Fragment() {
             startActivity(Intent.createChooser(intent, "Share To:"))
         }
     }
+
+    //Узнаем, было ли получено разрешение ранее
+    private fun checkPermission(): Boolean {
+        val result = ContextCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        )
+        return result == PackageManager.PERMISSION_GRANTED
+    }
+
+    //Запрашиваем разрешение
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            1
+        )
+    }
+
+    private fun saveToGallery(bitmap: Bitmap) {
+        val image_name = getLastSegmentOfUrl(picture.url)
+        val image_title = image_name.split(".")[0]
+
+
+        //Проверяем версию системы
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            //Создаем объект для передачи данных
+            val contentValues = ContentValues().apply {
+                //Составляем информацию для файла (имя, тип, дата создания, куда сохранять и т.д.)
+                put(MediaStore.Images.Media.TITLE, image_title)
+                put(MediaStore.Images.Media.DISPLAY_NAME,image_title)
+                put(MediaStore.Images.Media.MIME_TYPE, "image/jpeg")
+                put(MediaStore.Images.Media.DATE_ADDED,System.currentTimeMillis() / 1000)
+                put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis())
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/FilmsSearchApp")
+            }
+            //Получаем ссылку на объект Content resolver, который помогает передавать информацию из приложения вовне
+            val contentResolver = requireActivity().contentResolver
+            val uri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,contentValues)
+            //Открываем канал для записи на диск
+            val outputStream = contentResolver.openOutputStream(uri!!)
+            //Передаем нашу картинку, может сделать компрессию
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
+            //Закрываем поток
+            outputStream?.close()
+        } else {
+            //То же, но для более старых версий ОС
+            @Suppress("DEPRECATION")
+            MediaStore.Images.Media.insertImage(
+                requireActivity().contentResolver,
+                bitmap,
+                image_title,
+                image_title
+            )
+        }
+    }
+
+    private fun String.handleSingleQuote(): String {
+        return this.replace("'", "")
+    }
+
+    private fun performAsyncLoadOfPoster() {
+        //Проверяем есть ли разрешение
+        if (!checkPermission()) {
+            //Если нет, то запрашиваем и выходим из метода
+            requestPermission()
+            return
+        }
+        //Создаем родительский скоуп с диспатчером Main потока, так как будем взаимодействовать с UI
+        MainScope().launch {
+            //Включаем Прогресс-бар
+            binding.progressBar.isVisible = true
+            //Создаем через async, так как нам нужен результат от работы, то есть Bitmap
+            val job = scope.async {
+                detailsFragmentViewModel.loadWallpaper(picture.url)
+            }
+            //Сохраняем в галерею, как только файл загрузится
+            saveToGallery(job.await())
+            //Выводим снекбар с кнопкой перейти в галерею
+            Snackbar.make(
+                binding.root,
+                R.string.downloaded_to_gallery,
+                Snackbar.LENGTH_LONG
+            )
+                .setAction(R.string.open) {
+                    val intent = Intent()
+                    intent.action = Intent.ACTION_VIEW
+                    intent.type = "image/*"
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                    startActivity(intent)
+                }
+                .show()
+
+            //Отключаем Прогресс-бар
+            binding.progressBar.isVisible = false
+        }
+    }
+
+    fun getLastSegmentOfUrl(url: String): String {
+        // удаление параметров из URL-адреса
+        val urlWithoutParams = url.split("?")[0]
+        // разбиение URL-адреса на отдельные сегменты
+        val segments = urlWithoutParams.split("/")
+        // выбор последнего сегмента
+        return segments.last()
+    }
+
 
     override fun onDestroyView() {
         _binding = null
